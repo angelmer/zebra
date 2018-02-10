a7eff5394f0850b14b1c1de659d313d48c11d453
angelmer <maximer@mail.ru>
2018-02-10 18:06:54 +0300

    start to change header menu

--- js/main.js
+++ js/main.js
@@ -2,26 +2,15 @@
  * Created by merimax on 02.12.2016.
  */
 
-/*
- yandex map
-  */
-ymaps.ready(init);
+//header menu open close
 
-function init() {
-    var map = new ymaps.Map('map', {
-        center: [55.922394297631726, 37.749790123001034]
-        , zoom: 16.5
-        , controls: ['zoomControl']
-        , behaviors: ['drag']
-    });
-    var placemark = new ymaps.Placemark([55.922394297631726, 37.749790123001034], {
-        hintContent: 'multzebra'
-        , balloonContent: 'Детская студия мультипликации'
-    }, {
-        iconLayout: 'default#image'
-        , iconImageHref: 'img/google-clip-art-6.png'
-        , iconImageSize: [40, 40]
-        , iconImageOffset: [-10, -40]
-    });
-    map.geoObjects.add(placemark);
-}
+var headerMenuButton = document.getElementsByClassName('header__navigation-toggle');
+var headerMenu = document.getElementsByClassName('header__navigation-block');
+
+
+var menuToggle = function(event) {
+  event.preventDefault();
+
+};
+
+console.log();
