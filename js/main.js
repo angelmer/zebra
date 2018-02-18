24f7cc950c038128bb91471ca581da698d2b39d5
angelmer <maximer@mail.ru>
2018-02-18 15:33:24 +0300

    add carousel on index page

--- js/main.js
+++ js/main.js
@@ -28,8 +28,25 @@ toggler.addEventListener('click', function(event){
 
 });
 
-//open video on cartoons page
+//main preview slider
 
-var modalWindow = document.querySelector('.modal');
-var iframeWindow = document.getElementById('iframe');
+var slideIndex = 0;
+carousel();
 
+function carousel () {
+  var i;
+  var x = document.getElementsByClassName('main__preview-slide');
+
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+  }
+
+  slideIndex++;
+
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
+
+  x[slideIndex-1].style.display = 'block';
+  setTimeout(carousel, 2000);
+}
